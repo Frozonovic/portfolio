e9595c542c2d40245be34bca60f16f295986da59
glacialfrozone <blee20@georgefox.edu>
2025-01-31 11:10:26 -0800

    Updated fetch url

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,8 @@ import ProjectCard from 'c/ProjectCard'
 import Footer from 'c/Footer'
 
 const fetchProjects = async (): Promise<Project[]> => {
-  const response = await fetch('http://localhost:5000/api/github')
+  const url = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000"
+  const response = await fetch(`${url}/api/github`)
 
   if (!response.ok) {
     throw new Error(`Failed to fetch Data: ${response.statusText}`)
