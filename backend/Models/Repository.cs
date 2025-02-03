fd0d43dfc9ba91987126fc742ac59956f2586507
glacialfrozone <blee20@georgefox.edu>
2025-02-03 14:27:08 -0800

    Add constructor to Repository model and include Microsoft.Extensions.Http package

--- backend/Models/Repository.cs
+++ backend/Models/Repository.cs
@@ -7,5 +7,13 @@ namespace backend.Models
         public string? description { get; set; }
         public string svn_url { get; set; }
         public List<string> languages { get; set; } = new List<string>();
+
+        public Repository(int id, string name, string? description, string svn_url)
+        {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.svn_url = svn_url;
+        }
     }
 }
