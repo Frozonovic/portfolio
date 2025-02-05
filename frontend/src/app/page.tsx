8e828537234adb6fac93fe3c6f7a8ab1c0907533
glacialfrozone <blee20@georgefox.edu>
2025-02-05 13:00:41 -0800

    Another modification to url for private networking

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,5 @@
+'use server'
+
 import GitHubRepo from 't/GitHubRepo'
 import Image from 'next/image'
 import { Project } from 't/Project'
@@ -8,7 +10,7 @@ const fetchProjects = async (): Promise<Project[]> => {
   const url: string = process.env.NEXT_PUBLIC_INTERNAL || "http://localhost"
   const port: string = process.env.NEXT_PUBLIC_PORT || "5000"
 
-  const response = await fetch(`[http://${url}:${port}]/api/github`)
+  const response = await fetch(`http://[${url}:${port}]/api/github`)
 
   if (!response.ok) {
     throw new Error(`Failed to fetch Data: ${response.statusText}`)
