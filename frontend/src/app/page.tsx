e2938f19ed7e3374eb21d7ba7cd8eb31ac68c7bd
glacialfrozone <blee20@georgefox.edu>
2025-02-05 17:00:21 -0800

    Private Networking Attempt Log #23

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,41 +1,40 @@
+'use server'
+
 import Footer from 'c/Footer'
 import GitHubRepo from 't/GitHubRepo'
 import Image from 'next/image'
 import { Project } from 't/Project'
-import ProjectList from 'c/ProjectList'
+import ProjectCard from 'c/ProjectCard'
+import { Languages } from 'lucide-react'
 
 const fetchProjects = async (): Promise<Project[]> => {
-  const url: string = process.env.NEXT_PUBLIC_INTERNAL || "http://localhost"
-  const port: string = process.env.NEXT_PUBLIC_PORT || "5000"
+  const url: string = process.env.NEXT_PUBLIC_INTERNAL || 'http://localhost'
+  const port: string = process.env.NEXT_PUBLIC_PORT || '5000'
 
   console.log(`Fetching projects from: http://${url}:${port}/api/github`)
 
   const response = await fetch(`http://${url}:${port}/api/github`, {
-    mode: 'cors',
-    credentials: 'include'
+    headers: { 'Host': 'backend.railway.internal' },
+    cache: 'no-store'
   })
 
   if (!response.ok) {
-    throw new Error(`Failed to fetch Data: ${response.statusText}`)
+    throw new Error(`Failed to fetch data: ${response.statusText}`)
   }
 
   const data = await response.json()
 
-  if (!data) {
-    throw new Error('Received empty response')
-  }
-
   return data.map((p: GitHubRepo) => ({
     id: p.id,
     name: p.name,
-    description: p.description || "No description available",
+    description: p.description || 'No description available',
     languages: p.languages,
-    image: `/images/${(p.name).toLowerCase()}.png`,
-    svn_url: p.svn_url,
+    image: `/images/${p.name.toLowerCase()}.png`,
+    svn_url: p.svn_url
   }))
 }
 
-const ProjectsPage = async () => {
+export default async function ProjectsPage() {
   const projects = await fetchProjects()
 
   return (
@@ -68,17 +67,18 @@ const ProjectsPage = async () => {
       </section>
 
       {/* Project Grid */}
-      <section className="py-8">
-        <h2 className="section-header text-3xl font-bold flex items-center h-full justify-center mb-8">
-          My Projects
-        </h2>
-        {/* Pass projects data to a client component */}
-        <ProjectList projects={projects} />
+      <section className='py-8'>
+        <h2 className='section-header text-3xl font-bold flex items-center h-full justify-center mb-8'>My Projects</h2>
+        <div className='container mx-auto max-w-7xl px-6'>
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8'>
+            {projects.map((project) => (
+              <ProjectCard key={project.id} project={project} />
+            ))}
+          </div>
+        </div>
       </section>
 
       <Footer />
     </main>
   )
 }
-
-export default ProjectsPage
