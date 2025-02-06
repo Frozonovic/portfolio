fac174fc62cb3eb0b3d294abda4a016f864b4012
glacialfrozone <blee20@georgefox.edu>
2025-02-05 17:28:48 -0800

    Private Networking Attempt Log #29

--- backend/Models/GitHubRepositry.cs
+++ backend/Models/GitHubRepositry.cs
@@ -5,7 +5,7 @@ namespace backend.Models
         public required int Id { get; set; }
         public required string Name { get; set; }
         public string? Description { get; set; }
-        public required string Svn_Url { get; set; }
+        public required string Svn_url { get; set; }
         public required List<string> Languages { get; set; } = new List<string>();
     }
 }
