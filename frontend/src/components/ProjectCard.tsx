6a472b6b3870fde74b09ffcc1d60c1506f4a85f9
glacialfrozone <blee20@georgefox.edu>
2025-02-13 17:21:03 -0800

    Color palette testing

--- frontend/src/components/ProjectCard.tsx
+++ frontend/src/components/ProjectCard.tsx
@@ -38,7 +38,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
                         {project.languages.map((t) => (
                             <span
                                 key={t}
-                                className="text-sm font-medium rounded-full px-3 py-1 mr-2 mb-2"
+                                className="text-sm bg-gray-400 font-medium rounded-full px-3 py-1 mr-2 mb-2"
                             >
                                 {t}
                             </span>
@@ -48,7 +48,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
                 <div className="mt-4">
                     <Link
                         href={project.svn_url}
-                        className="inline-block w-full text-center px-5 py-2 rounded-lg font-semibold transition-colors"
+                        className="bg-blue-800 inline-block mt-4 px-5 py-2 rounded-lg font-semibold transition-colors"
                         target="_blank"
                         onClick={(e) => e.stopPropagation()}
                     >
